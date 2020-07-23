ca30b11b97b1b92f586a1a7236e58587708ee1b6
juraj_kapasny <juraj@knoyd.com>
2020-07-23 13:12:37 +0200

    adding solutions for week 1

---
+++ stream_tweets.py
@@ -0,0 +1,29 @@
+import twitter
+import os
+import json
+
+consumer_key = os.environ['twitter_consumer_key']
+consumer_secret = os.environ['twitter_consumer_secret']
+access_token = os.environ['twitter_access_token']
+access_token_secret = os.environ['twitter_access_secret']
+
+
+api = twitter.Api(consumer_key=consumer_key,
+                  consumer_secret=consumer_secret,
+                  access_token_key=access_token,
+                  access_token_secret=access_token_secret)
+
+
+FILTER = ['#datascience']
+LANGUAGES = ['en']
+
+
+def main(path, FILTER, LANGUAGES):
+    with open(path + 'output.txt', 'a') as f:
+        # api.GetStreamFilter will return a generator that yields one status
+        # message (i.e., Tweet) at a time as a JSON dictionary.
+        for line in api.GetStreamFilter(track=FILTER, languages=LANGUAGES):
+            f.write(json.dumps(line))
+            f.write('\n')
+            
+main(path='', FILTER=FILTER, LANGUAGES=LANGUAGES)
